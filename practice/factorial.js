806414f7372b18cf7294b064baecb89fc300a6b7
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-06-17 17:58:47 +0900

    factorial 2

--- practice/factorial.js
+++ practice/factorial.js
@@ -14,3 +14,11 @@ function Factorial(n) {
 };
 
 console.log(Factorial(3));
+
+/* 두번째 방법 */
+
+function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+  alert( factorial(5) ); // 120
